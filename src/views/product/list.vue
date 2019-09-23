824f39997da7e59d156457b8ce589bd3c95c2381
nidingbo <dingbo.ni@dtp1688.com>
2019-09-23 17:52:18 +0800

    9-23

--- src/views/product/list.vue
+++ src/views/product/list.vue
@@ -36,7 +36,7 @@
     <el-dialog
       title="提示"
       :visible.sync="dialogVisible"
-      width="40%"
+      width="60%"
       :before-close="handleClose">
       <add @mm = "dialogVisible = false"></add>
     </el-dialog>
